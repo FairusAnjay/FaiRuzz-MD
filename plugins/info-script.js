e454f30abdfe113540a7780be57cda2e69472ea0
FairusAnjayy <94828449+FairusAnjay@users.noreply.github.com>
2022-09-27 16:53:51 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,26 +2,14 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/channel/UC6GPl9xMWL61NAXQb3HBrRw*
-
-Pengen mentahan nya ?
-Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
-
-Mau Yang No Error Script Bot nya, Gas Cek Video YT :
-https://youtube.com/channel/UC6GPl9xMWL61NAXQb3HBrRw
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6285380166282`
+let str = `*Nyari Esce ya :v*`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/maxxy_ofc",
+    mediaUrl: "https://Instagram.com/fairuzzgapunyaduit",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: '𝑴𝒂𝒂𝒙𝒚 𝑩𝒐𝒕𝒛 MultiDevice',
+    description: "yg bc kek kntl", 
+    title: 'FaiRuzz +62 - カムレン.',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
